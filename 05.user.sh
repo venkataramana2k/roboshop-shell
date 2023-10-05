f302fe37e37de35279bc33011754a1f036d35b84
satya <satya.bhimuni123@gmail.com>
2023-10-05 10:56:00 +0530

    project

---
+++ 05.user.sh
@@ -0,0 +1,23 @@
+echo -e "\e[32m Downloading Nodejs repo file\e[0m"
+curl -SL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[32m Installing Nodejs server\e[0m"
+yum install nodejs -y
+echo - "\e[32m Adding user and location\e[0m"
+useradd roboshop
+mkdir /app
+cd /app
+echo -e "\e[32m Downloading new app content and dependencies to user server\e[0m"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/user.zip
+unzip user.zip
+rm -rf user.zip
+npm install
+echo -e "\e[32m creating user service file\e[0m"
+cp /root/roboshop-shell/user.service /etc/systemd/system/user.service
+echo -e "\e[32m Downloading and installing the mongodb schema\e[0m"
+cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.sadguru.shop.cloud </app/schema/user.js
+echo -e "\e[32m Enabling and starting the user service\e[0m"
+systemctl daemon-reload
+systemctl enable user
+systemctl restart user
