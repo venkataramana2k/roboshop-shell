cc96105c32fe107dd742e841472ef8011ee31fd2
satya <satya.bhimuni123@gmail.com>
2023-10-06 14:08:41 +0530

    bash

--- 08.shipping.sh
+++ 08.shipping.sh
@@ -4,10 +4,10 @@ echo -e "\e[32m Adding user and location\e[0m"
 useradd roboshop
 mkdir /app
 cd /app
+rm -rf *
 echo -e "\e[32m Downloading new app content to shipping server\e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 unzip shipping.zip
-rm -rf shipping.zip
 echo -e "\e[32m Downloading dependencies and building application to shipping server\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
