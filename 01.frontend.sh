5bd42df069751729eefb8edae830389155a1e25e
satya <satya.bhimuni123@gmail.com>
2023-10-11 09:39:34 +0530

    project

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,21 +1,20 @@
-color="\e[32m"
-nocolor="\e[0m"
-logfile=="/tmp/roboshop.log"
+source common.sh
+component=Nginx
 
-echo -e "$color Installing the Nginx server $nocolor"
-yum install nginx -y &>>$logfile
+echo -e "$color Installing the ${component} server $nocolor"
+yum install ${component} -y &>>$logfile
 echo -e "$color Removing default content $nocolor"
-cd /usr/share/nginx/html
+cd /usr/share/${component}/html
 rm -rf * &>>$logfile
-echo -e "$color Downloading new content to nginx server $nocolor"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "$color Downloading new content to ${component}$nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$logfile
 unzip frontend.zip &>>$logfile
 rm -rf frontend.zip
-echo -e "$color Configuring reverse proxy nginx server $nocolor"
-cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo -e "$color Enabling and starting the Nginx server $nocolor"
-systemctl enable nginx &>>$logfile
-systemctl restart nginx
+echo -e "$color Configuring reverse proxy ${component} server $nocolor"
+cp /root/roboshop-shell/roboshop.conf /etc/${component}/default.d/roboshop.conf
+echo -e "$color Enabling and starting the ${component} server $nocolor"
+systemctl enable ${component} &>>$logfile
+systemctl restart ${component}
 
 
 
