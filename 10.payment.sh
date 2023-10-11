b991d3ae35eb4dc059534fb3944c7d6a37a79ed5
satya <satya.bhimuni123@gmail.com>
2023-10-11 07:47:17 +0530

    project

--- 10.payment.sh
+++ 10.payment.sh
@@ -1,17 +1,22 @@
-echo -e "\e[32m Installing python server\e[0m"
-yum install python36 gcc python3-devel -y
-echo -e "\e[32m Adding user and Location\e[0m"
-useradd roboshop
-mkdir /app
+color="\e[32m"
+nocolor="\e[0m"
+logfile=="/tmp/payment.log"
+
+echo -e "$color Installing python server$nocolor"
+yum install python36 gcc python3-devel -y &>>$logfile
+echo -e "$color Adding user and Location$nocolor"
+useradd roboshop &>>$logfile
+mkdir /app &>>$logfile
 cd /app
 rm -rf *
-echo -e "\e[32m Downloading new app content and dependencies to payment server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-unzip payment.zip
-pip3.6 install -r requirements.txt
-echo -e "\e[32m creating payment service file\e[0m"
+echo -e "$color Downloading new app content and dependencies to payment server$nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>$logfile
+unzip payment.zip &>>$logfile
+pip3.6 install -r requirements.txt &>>$logfile
+echo -e "$color creating payment service file$nocolor"
 cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
-echo -e "\e[32m Enabling and starting the payment service\e[0m"
+echo -e "$color Enabling and starting the payment service$nocolor"
 systemctl daemon-reload
-systemctl enable payment
+systemctl enable payment &>>$logfile
 systemctl restart payment
+
