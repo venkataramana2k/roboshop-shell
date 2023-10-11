d04dfc190c34d9b703b8967286c11e2f084f7e29
satya <satya.bhimuni123@gmail.com>
2023-10-11 07:18:40 +0530

    project

--- 04.redis.sh
+++ 04.redis.sh
@@ -2,16 +2,17 @@ color="\e[33m"
 nocolor="\e[0m"
 logfile=="/tmp/redis.log"
 
-echo -e "\e[33m Downloading Redis repo\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/redis.log
-echo -e"\e[33m Enabling Redis-6.2 from package\e[0m"
-Yum module List &>>/tmp/redis.log
-yum module enable redis:remi-6.2 -y &>>/tmp/redis.log
-echo -e "\e[33m Installing Redis\e[0m"
-yum install redis -y &>>/tmp/redis.log
-echo -e "\e[32m Changing the listen address\e[0m"
+echo -e "$color Downloading Redis repo$nocolor"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$logfile
+echo -e"$color Enabling Redis-6.2 from package$nocolor"
+Yum module List &>>$logfile
+yum module enable redis:remi-6.2 -y &>>$logfile
+echo -e "$color Installing Redis$nocolor"
+yum install redis -y &>>$logfile
+echo -e "$color Changing the listen address$nocolor"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-echo -e "\e[32m Enabling and starting Redis server\e[0m"
-systemctl enable redis &>>/tmp/redis.log
+echo -e "$color Enabling and starting Redis server$nocolor"
+systemctl enable redis &>>$logfile
 systemctl restart redis
+
