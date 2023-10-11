d428cf0aeb00e24e2099b9cf7d16cee28e074ec8
satya <satya.bhimuni123@gmail.com>
2023-10-11 07:36:33 +0530

    project

--- 08.shipping.sh
+++ 08.shipping.sh
@@ -1,23 +1,27 @@
-echo -e "\e[32m Installing maven server\e[0m"
-yum install maven -y
-echo -e "\e[32m Adding user and location\e[0m"
-useradd roboshop
-mkdir /app
+color="$color"
+nocolor="\e[0m"
+logfile=="/tmp/shipping.log"
+
+echo -e "$color Installing maven server$nocolor"
+yum install maven -y &>>$logfile
+echo -e "$color Adding user and location$nocolor"
+useradd roboshop &>>$logfile
+mkdir /app &>>$logfile
 cd /app
 rm -rf *
-echo -e "\e[32m Downloading new app content to shipping server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-unzip shipping.zip
-echo -e "\e[32m Downloading dependencies and building application to shipping server\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[32m creating shipping service file\e[0m"
+echo -e "$color Downloading new app content to shipping server$nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>$logfile
+unzip shipping.zip &>>$logfile
+echo -e "$color Downloading dependencies and building application to shipping server$nocolor"
+mvn clean package &>>$logfile
+mv target/shipping-1.0.jar shipping.jar &>>$logfile
+echo -e "$color creating shipping service file$nocolor"
 cp /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[32m Downloading and installing the mysql schema\e[0m"
-yum install mysql -y
-mysql -h mysql-dev.sadguru.shop -uroot -pRoboShop@1 </app/schema/shipping.sql
-echo -e "\e[32m Enabling and starting the shipping service \e[0m"
+echo -e "$color Downloading and installing the mysql schema$nocolor"
+yum install mysql -y &>>$logfile
+mysql -h mysql-dev.sadguru.shop -uroot -pRoboShop@1 </app/schema/shipping.sql &>>$logfile
+echo -e "$color Enabling and starting the shipping service $nocolor"
 systemctl daemon-reload
-systemctl enable shipping
+systemctl enable shipping &>>$logfile
 systemctl restart shipping
 
