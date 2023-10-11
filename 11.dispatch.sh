ed9ed1e89d38bc68a78f4ae9e701e6e5c66de873
satya <satya.bhimuni123@gmail.com>
2023-10-11 07:54:20 +0530

    project

--- 11.dispatch.sh
+++ 11.dispatch.sh
@@ -1,21 +1,27 @@
-echo -e "\e[32m Installing golang server\e[0m"
-yum install golang -y
-echo -e "\e[32m Adding user and location\e[0m"
-useradd roboshop
-mkdir /app
+color="\e[32m"
+nocolor="\e[0m"
+logfile=="/tmp/dispatch.log"
+
+echo -e "$color Installing golang server$nocolor"
+yum install golang -y &>>$logfile
+echo -e "$color Adding user and location$nocolor"
+useradd roboshop &>>$logfile
+mkdir /app &>>$logfile
 cd /app
-echo -e "\e[32m Downloading new app content, dependencies and building software to dispatch server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-unzip dispatch.zip
-go mod init dispatch
-go get
-go build
-echo - "\e[32m creating dispatch service file\e[0"
+echo -e "$color Downloading new app content, dependencies and building software to dispatch server$nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>$logfile
+unzip dispatch.zip &>>$logfile
+go mod init dispatch &>>$logfile
+go get &>>$logfile
+go build &>>$logfile
+echo - "$color creating dispatch service file$nocolor"
 cp /root/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
-echo -e "\e[32m Enabling and starting the dispatch service\e[0m"
+echo -e "$color Enabling and starting the dispatch service$nocolor"
 systemctl daemon-reload
-systemctl enable dispatch
+systemctl enable dispatch &>>$logfile
 systemctl restart dispatch
+
+
 
 
 
