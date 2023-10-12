6636edfca4c57625e6abb6b7edd98d3e5d1d17d5
satya <satya.bhimuni123@gmail.com>
2023-10-12 16:05:12 +0530

    project

--- roboshop.sh
+++
@@ -1,5 +0,0 @@
-location /api/catalogue/ { proxy_pass http://catalogue-dev.sadguru.shop:8080/; }
-location /api/user/ { proxy_pass http://user-dev.sadguru.shop:8080/; }
-location /api/cart/ { proxy_pass http://cart-dev.sadguru.shop:8080/; }
-location /api/shipping/ { proxy_pass http://shipping-dev.sadguru.shop:8080/; }
-location /api/payment/ { proxy_pass http://payment-dev.sadguru.shop:8080/; }
