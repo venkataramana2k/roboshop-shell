e40b01bbfba56af530799269ea4a0012f5f0ae8a
satya <satya.bhimuni123@gmail.com>
2023-10-12 21:03:48 +0530

    project

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -3,14 +3,14 @@ nocolor="\e[0m"
 logfile=="/tmp/mysql.log"
 
 echo -e "$color Disabiling Mysql default version$nocolor"
-yum module disable mysql -y &>>$logfile
+yum module disable mysql -y &>>${logfile}
 echo -e "$color Setting up the MySQL5.7 repo file$nocolor"
 cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "$color Installing mysql server$nocolor"
-yum install mysql-community-server -y &>>$logfile
+yum install mysql-community-server -y &>>${logfile}
 echo -e "$color Changing default root password$nocolor"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>$logfile
+mysql_secure_installation --set-root-pass RoboShop@1 &>>${logfile}
 echo -e "$color Enabling and starting Mysql server$nocolor"
-systemctl enable mysqld &>>$logfile
+systemctl enable mysqld &>>${logfile}
 systemctl restart mysqld
 
