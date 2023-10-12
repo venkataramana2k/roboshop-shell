bb78c5075bd3265316946a638dde38ada894a426
satya <satya.bhimuni123@gmail.com>
2023-10-12 15:34:26 +0530

    project

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,7 +1,7 @@
 echo -e "\e[32m installing the Nginx server \e[0m"
 yum install nginx -y &>>/tmp/frontend.log
 echo -e "\e[32m Removing default content \e[0m"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html/*
 rm -rf * &>>/tmp/frontend.log
 echo -e "\e[32m Downloading new content to nginx server \e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/frontend.log
@@ -12,6 +12,7 @@ cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[32m Enabling and starting the Nginx server\e[0m"
 systemctl enable nginx &>>/tmp/frontend.log
 systemctl restart nginx
+
 
 
 
