699d1cfd9bdac4f87d582071cede627efff21b37
satya <satya.bhimuni123@gmail.com>
2023-10-12 21:06:08 +0530

    project

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -1,8 +1,8 @@
 color="\e[32m"
 nocolor="\e[0m"
-logfile=="/tmp/mysql.log"
+logfile="/tmp/mysql.log"
 
-echo -e "$color Disabiling Mysql default version$nocolor"
+echo -e "$color Disabling Mysql default version$nocolor"
 yum module disable mysql -y &>>${logfile}
 echo -e "$color Setting up the MySQL5.7 repo file$nocolor"
 cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
