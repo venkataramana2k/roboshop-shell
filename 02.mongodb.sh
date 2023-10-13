4ea0a8d8a88f1c42ca3c4288b1e67795609124e8
satya <satya.bhimuni123@gmail.com>
2023-10-13 10:42:49 +0530

    project

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -3,11 +3,11 @@ nocolor="\e[0m"
 logfile="/tmp/mongodb.log"
 
 echo -e "$color Downloading Mongodb Repo$nocolor"
-cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$logfile
+cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${logfile}
 echo -e "$color Installing Mongodb server$nocolor"
-yum install mongodb-org -y &>>$logfile
+yum install mongodb-org -y &>>${logfile}
 echo -e "$color Changing the listen address$nocolor"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "$color Enabling and starting Mongodb server$nocolor"
-systemctl enable mongod &>>$logfile
+systemctl enable mongod &>>${logfile}
 systemctl restart mongod
